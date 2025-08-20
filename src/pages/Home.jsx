c07513c776889ae2e0630e468f3442d1c9415ca2
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-20 11:14:37 +0200

    fetch-books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+    // vars
+    const [books, setBooks] = useState([]);
+    const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+    // function to get data
+    useEffect(() => {
+        fetch('https://gutendex.com/books')
+        .then(response => response.json())
+        .then(data => {
+            // Handle the fetched data here
+            setBooks(data.results);
+            setDataIsLoaded(true);
+
+            console.log(data.results);
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+    }, []);
+    // no data content
+    if (!dataIsLoaded) {
+        return (
+            <div>
+                <h1>Please wait some time....</h1>
+            </div>
+        );
+    }
+
+    // show if we have data
+    return (
+        <>
+            <div>Home</div>
+            <div>
+                <p>Book List</p>
+                <ul>
+                    {books.map(book => (
+                    <li key={book.id}>{book.title}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default Home
