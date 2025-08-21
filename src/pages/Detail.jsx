d67da8669b9c1ad4ca7fade495877689b85c9e47
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-21 13:23:05 +0200

    base-detail-page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
+
+    const { id } = useParams();
+    const urlData = "https://gutendex.com/books/";
+
+    const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+    const [book, setBook] = useState({});
+
+    // function to get data
+    useEffect(() => {
+        setDataIsLoaded(false);
+        fetch(urlData + id)
+        .then(response => response.json())
+        .then(data => {
+            // Handle the fetched data here
+            setBook(data);
+            setDataIsLoaded(true);
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+    }, []);
+    // no data content
+    if (!dataIsLoaded) {
+        return (
+            <div>
+                <h1>Please wait some time....</h1>
+            </div>
+        );
+    }
+
+    if (!book) return <p>Book not found.</p>;
+
+    return (
+        <>
+            <div>Detail</div>
+            <p>{book.title}</p>
+        </>
+    )
 }
 
 export default Detail
