5db325fd26c9edd77a67c0c350116db0632567bc
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-21 14:49:39 +0200

    detailed-book-page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Detail = () => {
 
@@ -11,6 +11,68 @@ const Detail = () => {
 
     const [book, setBook] = useState({});
 
+    const [author, setAuthor] = useState("");
+    const [category, setCategory] = useState("");
+    const [lang, setLang] = useState("");
+
+    // get string with author names
+    const getAuthors = (book) => {
+        let names = "";
+        const arr = book.authors;
+
+        for(let i = 0; i < arr.length; i++)
+        {
+            names += arr[i].name;
+
+            if(i + 1 < arr.length){
+                names += '; ';
+            }
+            else{
+                names += '.';
+            }
+        }
+
+        return names;
+    };
+    // get categories
+    const getCategories = (book) => {
+        let category = "";
+        const arr = book.bookshelves;
+
+        for(let i = 0; i < arr.length; i++)
+        {
+            category += arr[i];
+
+            if(i + 1 < arr.length){
+                category += '; ';
+            }
+            else{
+                category += '.';
+            }
+        }
+
+        return category;
+    }
+    // get languages
+    const getLanguages = (book) => {
+        let lang = "";
+        const arr = book.languages;
+
+        for(let i = 0; i < arr.length; i++)
+        {
+            lang += arr[i];
+
+            if(i + 1 < arr.length){
+                lang += ', ';
+            }
+            else{
+                lang += '.';
+            }
+        }
+
+        return lang;
+    }
+
     // function to get data
     useEffect(() => {
         setDataIsLoaded(false);
@@ -19,6 +81,9 @@ const Detail = () => {
         .then(data => {
             // Handle the fetched data here
             setBook(data);
+            setAuthor(getAuthors(data));
+            setCategory(getCategories(data));
+            setLang(getLanguages(data));
             setDataIsLoaded(true);
         })
         .catch(error => {
@@ -26,7 +91,8 @@ const Detail = () => {
             console.error('Error:', error);
         });
     }, [id]); // repeat on id is changed
-    // no data content
+
+    // while we are waiting
     if (!dataIsLoaded) {
         return (
             <div>
@@ -35,12 +101,21 @@ const Detail = () => {
         );
     }
 
+    // If we do not have a book with this id
     if (!book) return <p>Book not found.</p>;
 
+    // Main Content
     return (
         <>
             <div>Detail</div>
-            <p>{book.title}</p>
+            <p><b>Title:</b> {book.title}</p>
+            <img src={book.formats["image/jpeg"]} alt="Cover" />
+            <p><b>Author:</b> {author}</p>
+            <p><b>Number of downloads:</b> {book.download_count}</p>
+            <p><b>Category:</b> {category}</p>
+            <p><b>Language:</b> {lang}</p>
+            <p><b>Link to the book in digital format:</b></p>
+            <Link to={book.formats["text/plain; charset=us-ascii"]}>Link</Link>
         </>
     )
 }
